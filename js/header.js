166fab53ceed6d40fc77b80be20cfab4846e3109
Shubham <sv7836832607@gmail.com>
2022-02-25 12:32:28 +0530

    <>

--- js/header.js
+++ js/header.js
@@ -1,29 +1,29 @@
 function mainpage() {
-  window.location.href = "header.html";
+    window.location.href = "feature.html";
 }
 
 function lab_test_page() {
-  window.location.href = "header.html";
+    window.location.href = "lab.html";
 }
 
 function ask_doc_page() {
-  window.location.href = "AskDoctor.html";
+    window.location.href = "AskDoctor.html";
 }
 
 function covid_page() {
-  window.location.href = "header.html";
+    window.location.href = "header.html";
 }
 
 function ayurveda_page() {
-  window.location.href = "header.html";
+    window.location.href = "header.html";
 }
 
 function care_plan_page() {
-  window.location.href = "header.html";
+    window.location.href = "header.html";
 }
 
 function cart_page() {
-  window.location.href = "Cart.html";
+    window.location.href = "Cart.html";
 }
 
 // if (localStorage.getItem(JSON.parse("cartdata")).length == 0) {
